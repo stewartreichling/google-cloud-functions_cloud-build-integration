fb70ab19a8596b4560c667b9574811dd5fd1dc90
Stewart Reichling <stewart.reichling@gmail.com>
2018-11-12 12:57:13 -0800

    Add Hello, World Node.js function

---
+++ autodeploy/index.js
@@ -0,0 +1,11 @@
+/**
+ * HTTP Cloud Function.
+ *
+ * @param {Object} req Cloud Function request context.
+ *                     More info: https://expressjs.com/en/api.html#req
+ * @param {Object} res Cloud Function response context.
+ *                     More info: https://expressjs.com/en/api.html#res
+ */
+exports.helloHttp = (req, res) => {
+  res.send(`Hello ${req.body.name || 'World'}!`);
+};
